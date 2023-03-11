3bb1642b9875612bba6c98fa9e3c0e1fa2e04838
Rodrigo Álvarez <papipo@gmail.com>
2023-03-11 23:59:30 +0100

    Add seeding.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,20 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Remote.{User, Repo}
+
+Repo.delete_all(User, log: false)
+
+timestamp = NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second)
+
+IO.puts("Inserting 1 million users...")
+
+Stream.cycle([%{points: 0, inserted_at: timestamp, updated_at: timestamp}])
+|> Stream.take(1_000_000)
+# the maximum parameters postgresql protocol can handle is 65535
+|> Stream.chunk_every(20000)
+|> Stream.map(&Task.async(fn -> Repo.insert_all(User, &1, log: false) end))
+|> Enum.each(&Task.await/1)
+
+IO.puts("Done.")
